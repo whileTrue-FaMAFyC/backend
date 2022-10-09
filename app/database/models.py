87094e4c2eb08f40a247a58bcf80b8a8b9332b26
tonimondejar <antonio.mondejar@mi.unc.edu.ar>
2022-10-09 11:59:29 -0300

    PYR-12_database_design_corrected_and_binding

--- app/database/models.py
+++ app/database/models.py
@@ -5,37 +5,37 @@ db = Database()
 
 
 class User(db.Entity):
-    username = PrimaryKey(str, auto=True)
-    email = Required(str, unique=True)
+    username = Required(str)
+    email = PrimaryKey(str)
     avatar = Optional(buffer)
     password = Required(str)
-    verificationCode = Required(int)
+    verification_code = Required(int)
     verified = Required(bool)
     robots = Set('Robot')
-    matchsCreated = Set('Match', reverse='creatorUser')
-    matchsJoined = Set('Match', reverse='usersJoined')
+    matches_created = Set('Match')
 
 
 class Robot(db.Entity):
     name = Required(str)
     owner = Required(User)
     avatar = Optional(buffer)
-    matchsJoined = Set('Match')
+    matches_joined = Set('Match')
+    source_code = Required(buffer)
     PrimaryKey(name, owner)
 
 
 class Match(db.Entity):
     name = Required(str)
-    creatorUser = Required(User, reverse='matchsCreated')
-    usersJoined = Set(User, reverse='matchsJoined')
-    robotsJoined = Set(Robot)
-    minPlayers = Required(int)
-    maxPlayers = Required(int)
-    numGames = Required(int)
-    numRounds = Required(int)
+    creator_user = Required(User)
+    robots_joined = Set(Robot)
+    min_players = Required(int)
+    max_players = Required(int)
+    num_games = Required(int)
+    num_rounds = Required(int)
     started = Required(bool)
-    PrimaryKey(name, creatorUser)
+    password = Optional(str)
+    PrimaryKey(name, creator_user)
 
+db.bind(provider='sqlite', filename='database.sqlite', create_db=True)
 
-
-db.generate_mapping()
+db.generate_mapping(create_tables=True)
