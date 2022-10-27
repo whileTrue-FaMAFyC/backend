7321130da4805a96cbb206e6e69da7d6074b40d1
basbenja <79337950+basbenja@users.noreply.github.com>
2022-10-27 09:39:07 -0300

    PYR-98_removed-unnecessary-line

--- app/utils/robot_utils.py
+++ app/utils/robot_utils.py
@@ -36,8 +36,7 @@ ROBOT_DB_EXCEPTION = HTTPException(
 # sent to the frontend.
 @db_session
 def robot_db_to_view(robots: Robot):
-    users_robots = [ShowRobot.from_orm(r) for r in robots]
-    return users_robots
+    return [ShowRobot.from_orm(r) for r in robots]
 
 
 def insert_filename_to_file(file: str, filename: str):
