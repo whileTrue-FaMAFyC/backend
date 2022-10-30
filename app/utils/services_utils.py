9fc2f2fdeb85eb571dc104e08d4ed090289c6b04
basbenja <79337950+basbenja@users.noreply.github.com>
2022-10-30 19:41:43 -0300

    PYR-74_code-from-other-branch-removed

--- app/utils/services_utils.py
+++ app/utils/services_utils.py
@@ -25,28 +25,3 @@ class GameException(Exception):
     def __init__(self, detail):
         self.message = detail
         super().__init__(self.message)
-
-def extract_filename_from_file(source_code_in_db):
-    filename = source_code_in_db.split(";")[0].replace("name:", "")
-    source_code = source_code_in_db.split("base64,")[1]
-    return filename, source_code
-
-def extract_class_name(filename):
-    without_ = filename.split("_")
-    for i in range(len(without_)):
-        without_[i] = without_[i].capitalize()
-    class_name = ''.join(without_)
-    return class_name[:len(class_name)-3]
-
-def create_robots_instances(robots_id: List[int]):
-    robots = []
-    robot = ""
-    for r in robots_id:
-        source_code_in_db = get_source_code_by_id(r)
-        filename, source_code_b64 = extract_filename_from_file(source_code_in_db)
-        source_code = IMPORT_ROBOT_CLASS + b64decode(source_code_b64)
-        exec(source_code)
-        class_name = extract_class_name(filename)
-        exec(f"robot = {class_name}(robot_id={r})")
-        robots.append(robot)
-    return robots
