f65523e2615df78bae9f3ca9ff9dc57d4f7d944b
Julia Olcese <julia.olcese@mi.unc.edu.ar>
2022-11-03 09:16:37 -0300

    HOTFIX_develop_change-meters-per-velocity-percentage

--- app/testing/tests/test_PYR_76_simulation_execution.py
+++ app/testing/tests/test_PYR_76_simulation_execution.py
@@ -6,45 +6,45 @@ from testing.helpers.generate_token import MOCK_TOKEN_BENJA
 
 client = TestClient(app)
 
-# def test_invalid_rounds():
-#     response = client.post(
-#         "/new-simulation",
-#         headers = {"authorization": MOCK_TOKEN_BENJA},
-#         json = {
-#             "num_rounds": 1000000,
-#             "robots": ["Bumblebee", "0ptimusPrime"]
-#         }
-#     )
+def test_invalid_rounds():
+    response = client.post(
+        "/new-simulation",
+        headers = {"authorization": MOCK_TOKEN_BENJA},
+        json = {
+            "num_rounds": 1000000,
+            "robots": ["Bumblebee", "0ptimusPrime"]
+        }
+    )
 
-#     assert response.status_code == 400
-#     assert response.json()["detail"] == "Number of rounds has to be between 1 and 10000. "
+    assert response.status_code == 400
+    assert response.json()["detail"] == "Number of rounds has to be between 1 and 10000. "
 
 
-# def test_not_creators_robot():
-#     response = client.post(
-#         "/new-simulation",
-#         headers = {"authorization": MOCK_TOKEN_BENJA},
-#         json = {
-#             "num_rounds": 100,
-#             "robots": ["Bumblebee", "0ptimusPrime", "invalid"]
-#         }
-#     )
+def test_not_creators_robot():
+    response = client.post(
+        "/new-simulation",
+        headers = {"authorization": MOCK_TOKEN_BENJA},
+        json = {
+            "num_rounds": 100,
+            "robots": ["Bumblebee", "0ptimusPrime", "invalid"]
+        }
+    )
 
-#     assert response.status_code == 409
-#     assert response.json()["detail"] == "Robot invalid isn't in bas_benja's library. "
+    assert response.status_code == 409
+    assert response.json()["detail"] == "Robot invalid isn't in bas_benja's library. "
 
-# def test_invalid_robot_amount():
-#     response = client.post(
-#         "/new-simulation",
-#         headers = {"authorization": MOCK_TOKEN_BENJA},
-#         json = {
-#             "num_rounds": 100,
-#             "robots": ["Bumblebee"]
-#         }
-#     )
+def test_invalid_robot_amount():
+    response = client.post(
+        "/new-simulation",
+        headers = {"authorization": MOCK_TOKEN_BENJA},
+        json = {
+            "num_rounds": 100,
+            "robots": ["Bumblebee"]
+        }
+    )
 
-#     assert response.status_code == 400
-#     assert response.json()["detail"] == "The simulation needs between 2 and 4 robots. "
+    assert response.status_code == 400
+    assert response.json()["detail"] == "The simulation needs between 2 and 4 robots. "
 
 def test_successful_creation():
     response = client.post(
@@ -64,19 +64,19 @@ def test_successful_creation():
     ]
 
 
-# def test_repeated_robot():
-#     response = client.post(
-#         "/new-simulation",
-#         headers = {"authorization": MOCK_TOKEN_BENJA},
-#         json = {
-#             "num_rounds": 100,
-#             "robots": ["Bumblebee", "Bumblebee"]
-#         }
-#     )
+def test_repeated_robot():
+    response = client.post(
+        "/new-simulation",
+        headers = {"authorization": MOCK_TOKEN_BENJA},
+        json = {
+            "num_rounds": 100,
+            "robots": ["Bumblebee", "Bumblebee"]
+        }
+    )
 
-#     assert response.status_code == 201
-#     assert response.json()["names"] == [{"name": "Bumblebee",
-#                                          "id": 0},
-#                                         {"name": "Bumblebee",
-#                                          "id": 1}
-#     ]
+    assert response.status_code == 201
+    assert response.json()["names"] == [{"name": "Bumblebee",
+                                         "id": 0},
+                                        {"name": "Bumblebee",
+                                         "id": 1}
+    ]
